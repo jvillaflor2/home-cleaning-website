7107ca19964d141f464c2cfd83f5afd7378398c0
jvillaflor2 <157553901+jvillaflor2@users.noreply.github.com>
2026-09-01 13:49:35 -0600

    feat: persist booking submissions to JSON file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const fs = require('fs');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
@@ -8,8 +9,24 @@ app.use(express.json());
 
 app.post('/booking', (req, res) => {
     const booking = req.body;
-    console.log('New booking request', booking);
-    res.send('Thank you! We recieved your request and will contact you shortly.');
+
+    fs.readFile('data/bookings.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading file:', err);
+      return res.status(500).send('Something went wrong.');
+    }
+
+    const bookings = JSON.parse(data);
+    bookings.push(booking);
+
+    fs.writeFile('data/bookings.json', JSON.stringify(bookings, null, 2), (err) => {
+      if (err) {
+        console.error('Error writing file:', err);
+        return res.status(500).send('Something went wrong.');
+      }
+      res.send('Thanks! We received your request and will contact you shortly.');
+    });
+    });
 });
 
 app.listen(PORT, () => {
