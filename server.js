3676805f427d27bcc42cb91d2b13e2a06bdabab3
jvillaflor2 <157553901+jvillaflor2@users.noreply.github.com>
2026-08-31 20:42:47 -0600

    feat: add booking form and POST route to capture submissions

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post('/booking', (req, res) => {
+    const booking = req.body;
+    console.log('New booking request', booking);
+    res.send('Thank you! We recieved your request and will contact you shortly.');
+});
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
