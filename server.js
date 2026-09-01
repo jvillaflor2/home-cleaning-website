312644b7bff19aff47f769da7a2fb0aa99ea676b
jvillaflor2 <157553901+jvillaflor2@users.noreply.github.com>
2026-08-31 20:18:29 -0600

    feat: serve static homepage from public folder

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get ('/', (req, res) => {
-    res.send('Hello World - cleaning service site coming soon!');
-});
+app.use(express.static('public'));
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
